775dcf936909998e9cf532644bf077f06d05504f
OperationalService <93121631+OperationalService@users.noreply.github.com>
2021-10-26 01:02:36 +0300

    Add files via upload

---
+++ hack_itcamp-master/client/src/main/ts/client/dist/precache-manifest.d4663d2d2427805f79a04e7a5d546071.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ffbf6c87f66217c61e",
+    "url": "/css/about.1197c15d.css"
+  },
+  {
+    "revision": "c8557ff55e929dd09c34",
+    "url": "/css/app.cc3cb3d2.css"
+  },
+  {
+    "revision": "3d2d289b42076ee0a9d0cd1cedfdc7f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ffbf6c87f66217c61e",
+    "url": "/js/about.f3cd763e.js"
+  },
+  {
+    "revision": "c8557ff55e929dd09c34",
+    "url": "/js/app.be2c5f41.js"
+  },
+  {
+    "revision": "436c9ce2d68be1b35b8b",
+    "url": "/js/chunk-vendors.e218d6f3.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
